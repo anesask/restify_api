03ab23fe34e69db0fd4ed43dbbe543c4e0517c1c
Anes Mulalic <anes.mulalic@hotmail.com>
2020-07-06 22:59:41 +0200

    User - Basic Auth

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
 
 server.listen(config.PORT, () => {
-  mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.set("useFindAndModify", false);
+  mongoose.connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 });
 
 const db = mongoose.connection;
 db.on("error", (err) => console.log(err));
 db.once("open", () => {
-    require('./routes/customers')(server);
-    console.log(`Server started on port ${config.PORT}`.underline.bold.green);
+  require("./routes/customers")(server);
+  require("./routes/users")(server);
+  console.log(
+    `Server started on port `.bold.green +
+      `${config.PORT}`.underline.bold.yellow
+  );
 });
---
+++ routes/users.js
@@ -0,0 +1,43 @@
+const errors = require("restify-errors");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const auth = require("../auth");
+module.exports = (server) => {
+  // Register User
+  server.post("/register", (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = new User({
+      email,
+      password,
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, async (err, hash) => {
+        // Hash Password
+        user.password = hash;
+        // Save User
+        try {
+          const newUser = await user.save();
+          res.send(201);
+          next();
+        } catch (err) {
+          return next(new errors.InternalError(err.message));
+        }
+      });
+    });
+  });
+
+  // Auth User
+  server.post("/auth", async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      // Authenticate User
+      const user = await auth.authenticate(email, password);
+      console.log(user);
+      next();
+    } catch (err) {
+      return next(new errors.UnauthorizedError(err));
+    }
+  });
+};
